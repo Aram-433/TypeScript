a3881ab93ee2a7f4ae8664a7b66c25fbf1f99c25
Aram Harutyunyan <aram.harutyunyan433@gmail.com>
2025-02-05 14:18:21 +0400

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import { Task } from "./types/Task";
+import ToDoList from "./components/ToDoList";
+import styles from "./App.module.css";
+
+const App: React.FC = () => {
+  const [todos, setTodos] = useState<Task[]>([]);
+  const [done, setDone] = useState<number>(0);
+
+  useEffect(() => {
+    setDone(todos.filter((a) => a.done).length);
+  }, [todos]);
+
+  const addTask = (task: Task): void => {
+    setTodos([task, ...todos]);
+  };
+
+  const completeTask = (task: Task): void => {
+    task.done = true;
+    task.timeCompleted = Date.now();
+    setTodos([...todos]);
+  };
+
+  const cancelTask = (task: Task): void => {
+    task.done = false;
+    task.timeCompleted = null;
+    setTodos([...todos]);
+  };
+
+  return (
+    <div className={styles.App}>
+      <h3>TO DO LIST</h3>
+      <ToDoList
+        tasks={todos}
+        done={done}
+        onAdd={addTask}
+        onComplete={completeTask}
+        onCancel={cancelTask}
+      />
+    </div>
+  );
+};
+
+export default App;
